b74195caf518fdb5bf6586a0c2dcc271dded48e1
smdeile <smdeile@gmail.com>
2020-12-15 16:20:33 +0200

    Fix favorite movie list

--- src/services/get-movies.js
+++ src/services/get-movies.js
@@ -1,4 +1,5 @@
-const moviesAPI = 'http://my-json-server.typicode.com/moviedb-tech/movies/list';
+const moviesAPI =
+  'https://cors-anywhere.herokuapp.com/http://my-json-server.typicode.com/moviedb-tech/movies/list';
 export default {
   async fetchArticles() {
     return fetch(moviesAPI).then(response => response.json());
